de8e5e09390ba65bc1d042b4f7c6ed707a9f1f6c
Felipe <koutodev@gmail.com>
2024-04-19 10:56:09 -0300

    Alteração de class para className

--- src/App.js
+++ src/App.js
@@ -6,47 +6,47 @@ function App() {
     <main className="App">
       {/* <!-- Header --> */}
       <header className="container-fluid ">
-        <nav class="navbar-nav p-2 p-sm-3 p-lg-4">
-          <div class="row">
+        <nav className="navbar-nav p-2 p-sm-3 p-lg-4">
+          <div className="row">
             {/* <!-- Coluna 1 --> */}
-            <div class="col align-self-center">
+            <div className="col align-self-center">
               {/* <!-- Logo --> */}
-              <a href="#" class="navbar-brand">
+              <a href="#" className="navbar-brand">
                 AnkiDuckie
               </a>
             </div>
             {/* <!-- Coluna 2 --> */}
-            <div class="col-auto">
-              <div class="row row-cols-2 gap-2">
-                <div class="col-auto p-0 m-0">
-                  <button class="btn btn-primary">Cadastrar</button>
+            <div className="col-auto">
+              <div className="row row-cols-2 gap-2">
+                <div className="col-auto p-0 m-0">
+                  <button className="btn btn-primary">Cadastrar</button>
                 </div>
-                <div class="col-auto p-0 m-0">
-                  <button class="btn btn-outline-primary">Login</button>
+                <div className="col-auto p-0 m-0">
+                  <button className="btn btn-outline-primary">Login</button>
                 </div>
               </div>
             </div>
           </div>
         </nav>
       </header>
-      <section class="container-fluid align-content-center py-2 container-principal">
-        <div class="row">
-          <div class="col-12 py-4">
-            <div class="row text-center">
-              <div class="col-12">
-                <h1 class="display-4 fw-bold text-light">
+      <section className="container-fluid align-content-center py-2 container-principal">
+        <div className="row">
+          <div className="col-12 py-4">
+            <div className="row text-center">
+              <div className="col-12">
+                <h1 className="display-4 fw-bold text-light">
                   Aprenda com flashcards, <br /> estude menos!
                 </h1>
               </div>
-              <div class="col-12">
-                <p class="lead fw-normal text-light">
+              <div className="col-12">
+                <p className="lead fw-normal text-light">
                   Esqueça o medo das provas e aprenda idiomas com facilidade!{" "}
                   <br />A repetição espaçada, com comprovação científica, é a
                   sua aliada.
                 </p>
               </div>
-              <div class="col-12">
-                <button class="btn btn-primary text-uppercase">
+              <div className="col-12">
+                <button className="btn btn-primary text-uppercase">
                   Começar Agora
                 </button>
               </div>
@@ -54,16 +54,16 @@ function App() {
           </div>
         </div>
       </section>
-      <section class="container py-4">
-        <div class="row">
-          <div class="col-12 col-lg-6">
+      <section className="container py-4">
+        <div className="row">
+          <div className="col-12 col-lg-6">
             <img
-              class="img-fluid"
+              className="img-fluid"
               src="https://assets-global.website-files.com/6257adef93867e50d84d30e2/6582c18a9cff186bd3731704_Create%20an%20invite-only%20place%20where%20you%20belong.svg"
               alt=""
             ></img>
           </div>
-          <div class="col-12 col-lg-6 align-self-center">
+          <div className="col-12 col-lg-6 align-self-center">
             <h3 className="fw-bolder">
               Esquecer? Jamais! A era da memória impecável chegou!
             </h3>
