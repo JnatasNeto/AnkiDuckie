62828137d1b501f47424c0be58388bc8df99e070
Felipe <koutodev@gmail.com>
2024-04-19 15:28:45 -0300

    identação Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,15 +1,13 @@
-function Footer(){
-    return(
-        <footer className="container-fluid" >
-            <div className="row">
-            <div className="col align-self-center container text-center">
-              <a>
-                AnkiDuckie &copy; Desenvolvido em 2024
-              </a>
-            </div>
-          </div>
-        </footer>
-    );
+function Footer() {
+  return (
+    <footer className="container-fluid p-4">
+      <div className="row">
+        <div className="col align-self-center container text-center">
+          <a>AnkiDuckie&copy; Desenvolvido em 2024</a>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
